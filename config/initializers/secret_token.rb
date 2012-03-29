7dcbc47a9798838ecedaf2e4a022e4fc9b19d8b4
Mihai Florea <fmihai1990@yahoo.com>
2012-03-29 14:55:32 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Porcoupon::Application.config.secret_token = '85fef6e6b20b9ed7246c4c569780b16d12abf8ba9bdec9f5f9f9002eaa8b95f84f0f8838b7674b0f5338becd73001a84ba3b67825c77c7c57fa60d1b0a26c26e'
