3de02509138d0138b79220df4e86a7db58bf9b9d
Mihai Florea <fmihai1990@yahoo.com>
2012-05-04 14:03:40 +0300

    ...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
 	def set_access_control_headers
 		headers['Access-Control-Allow-Origin'] = '*'
 		headers['Access-Control-Request-Method'] = '*'
+		headers['Access-Control-Allow-Headers'] = '*'	
 	end
 end
