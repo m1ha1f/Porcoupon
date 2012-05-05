1f2c84c6ac19437bf1e47c1f9ab493036aaba09d
igsi <igsi16@yahoo.com>
2012-05-05 20:16:46 +0300

    improved grid funcionality

--- app/controllers/webservice_controller.rb
+++ app/controllers/webservice_controller.rb
@@ -4,6 +4,7 @@ class WebserviceController < ApplicationController
 		@title = "Webservice"
 		deal = Coupon.order("RANDOM()").first
 		response.content_type = Mime::JSON
-		render :text => deal.to_json(:only => [:title, :text, :image_url])
+		a = {:i => params[:i], :title => deal.title, :text => deal.text, :image_url => deal.image_url, :price => deal.formatted_price }
+		render :text => a.to_json()
 	end
 end
